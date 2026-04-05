d32f7d88fb20d6a0c33b45e8faf3d852c447a6f6
Andrey Kabanov <a.kabanov1983@gmail.com>
2026-04-05 10:02:09 +0300

    Изменения в ДЗ 3

--- Lecture 3/Andrey_Kabanov_home_work_L3.py
+++ Lecture 3/Andrey_Kabanov_home_work_L3.py
@@ -1,24 +1,29 @@
 # # Home work 3.1
-#
 # hat_list = [1, 2, 3, 4, 5]
+# print(f'Исходный список: {hat_list}')
 #
-# # Step 0: length of list
-# a = len(hat_list)
+# # Step 0: length of list.
+# len_hat_list = len(hat_list)
 #
-# print(a)
+# print(f'Длина исходного списка: {len_hat_list} ')
+#
+# # Look for middle element of the list
+# middle_index = len_hat_list // 2
+#
+# print(f'Значение среднего элемента списка: {hat_list[middle_index]}')
 #
 # # Step 1: replace the middle number entered by the user.
-# hat_list[2] = int(input
+# hat_list[middle_index] = int(input
 #                ('Введите целое число для замены среднего элемента списка: '))
 #
 #
-# # Step 2: remove the iast element from the list.
+# # Step 2: remove the last element from the list.
 # hat_list.pop()
 #
-# # Step 3: length of the existing list
-# print(len(hat_list))
+# # Step 3: length of the existing list.
+# print(f'Длина списка после удаленя последнего элемента: {len(hat_list)}')
 #
-# print(hat_list)
+# print(f'Список после изменений: {hat_list}')
 
 
 # # Home work 3.4 (lab)
@@ -34,12 +39,12 @@
 #         res_list.append(elem)
 #
 # print(res_list)
-
-
-# # Home work 3.5
 #
+#
+# # Home work 3.5
 # li = input('Введите целые числа через пробел: ').split()
 #
+# # Convert str to int type.
 # li_int = [int(i) for i in li]
 #
 # print(f'Сумма введенных чисел: {sum(li_int)}')
