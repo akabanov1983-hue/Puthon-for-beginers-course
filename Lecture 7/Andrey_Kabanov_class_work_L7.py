9a3a9ab950d1d52e1a15d154253fe7b54864cd55
Andrey Kabanov <a.kabanov1983@gmail.com>
2026-04-30 16:59:02 +0300

    Обновления по ДЗ

--- Lecture 7/Andrey_Kabanov_class_work_L7.py
+++ Lecture 7/Andrey_Kabanov_class_work_L7.py
@@ -1,12 +1,12 @@
 import pygame
 
 run= True
-w = 400
+w = 600
 h = 100
 pygame.init()
 sc= pygame.display.set_mode((w, h))
 f = pygame.font.SysFont(None, 48)
-t = f.render("Welcome to pygame!)", True, (255, 255, 255))
+t = f.render("Все работает! И я не Петр!", True, (255, 255, 255))
 sc.blit(t, ((w -t.get_width()) // 2, (h -t.get_height())))
 pygame.display.flip()
 while run:
